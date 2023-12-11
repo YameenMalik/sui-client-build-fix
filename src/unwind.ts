ce73df2849e8aa3f35a3fe06b6ff5e932a2775ec
YameenMalik <yameen@seed.im>
2023-12-12 01:21:41 +0500

    improved unwinding

--- src/unwind.ts
+++ src/unwind.ts
@@ -1,29 +1,54 @@
 /* eslint-disable prettier/prettier */
 import { ORDER_SIDE, ORDER_TYPE } from "@firefly-exchange/library-sui";
-import { BluefinClient, Networks } from "@bluefin-exchange/bluefin-v2-client";
+import {
+  BluefinClient,
+  Networks,
+  OnChainCalls,
+  SuiClient,
+  getSignerFromSeed,
+  readFile,
+  toBaseNumber,
+} from "@bluefin-exchange/bluefin-v2-client";
+const deployment = readFile("./deployment.json");
+
+const client = new SuiClient({ url: "https://fullnode.mainnet.sui.io:443" });
+
+const liquidator = getSignerFromSeed(
+  "royal reopen journey royal enlist vote core cluster shield slush hill sample",
+  "Secp256k1"
+);
+
+const onChain = new OnChainCalls(liquidator, deployment, client);
 
 const main = async () => {
-  const client = new BluefinClient(
-    true,
-    Networks.TESTNET_SUI, // select network
-    "royal reopen journey royal enlist vote core cluster shield slush hill sample",
-    "Secp256k1"
-  );
+  const client = new BluefinClient(true, Networks.PRODUCTION_SUI, liquidator);
 
   await client.init();
 
   console.log("Account address:", client.getPublicAddress());
 
-  const signedOrder = await client.createSignedOrder({
-    symbol: "BTC-PERP",
-    price: 0, // market order
-    quantity: 0.439, // should be user position size
-    side: ORDER_SIDE.SELL, // should be inverse of user's current position
-    orderType: ORDER_TYPE.MARKET,
-    leverage: 2,
-  });
+  for (const market of ["ETH-PERP", "BTC-PERP"]) {
+    try {
+      const { isPosPositive, qPos } = await onChain.getUserPosition(market);
+      const size = toBaseNumber(qPos, 3, 9);
+      console.log({ market, isPosPositive, size });
 
-  console.dir(await client.placeSignedOrder(signedOrder), null);
+      if (size > 0) {
+        const signedOrder = await client.createSignedOrder({
+          symbol: market,
+          price: 0, // market order
+          quantity: size,
+          side: isPosPositive == true ? ORDER_SIDE.SELL : ORDER_SIDE.BUY,
+          orderType: ORDER_TYPE.MARKET,
+          leverage: 2,
+        });
+
+        console.dir(await client.placeSignedOrder(signedOrder), null);
+      }
+    } catch (e) {
+      console.log({ market, isPosPositive: false, size: 0 });
+    }
+  }
 };
 
 main();
