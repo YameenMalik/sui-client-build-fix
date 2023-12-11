b523791ba083d6f7e167cc6f76cc53801990ec04
YameenMalik <yameen@seed.im>
2023-12-11 21:20:28 +0500

    client bump

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,33 @@
-import { BluefinClient, Networks } from "@bluefin-exchange/bluefin-v2-client";
+import {
+  BluefinClient,
+  Ed25519Keypair,
+  Keypair,
+  Networks,
+  ORDER_SIDE,
+  ORDER_TYPE,
+  OrderSigner,
+} from "@bluefin-exchange/bluefin-v2-client";
 
 async function main() {
-  const client = new BluefinClient(true, Networks["PRODUCTION_SUI"]);
+  const seed =
+    "royal reopen journey royal enlist vote core cluster shield slush hill sample";
+
+  const kp = Ed25519Keypair.deriveKeypair(seed);
+
+  const client = new BluefinClient(true, Networks["TESTNET_SUI"], kp);
+
+  await client.init(true);
+
+  const signedOrder = await client.createSignedOrder({
+    symbol: "ETH-PERP",
+    price: 0,
+    quantity: 0.02,
+    side: ORDER_SIDE.SELL,
+    orderType: ORDER_TYPE.MARKET,
+    leverage: 3,
+  });
+
+  console.dir(await client.placeSignedOrder(signedOrder), null);
 }
 
 main();
